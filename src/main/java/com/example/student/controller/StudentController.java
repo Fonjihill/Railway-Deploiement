c5b33d86fbbeb65588cbc5bc33c001dd67fe0601
Fonji Terence <terencehilln@gmail.com>
2023-07-26 09:13:30 +0200

    docker-compose.yml update

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -2,8 +2,13 @@ package com.example.student.controller;
 
 
 import com.example.student.model.entity.Student;
+import com.example.student.model.view.StudentView;
 import com.example.student.repository.StudentRepository;
 import com.example.student.service.StudentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Api(value="StudentController", description="Rest API for a Student.")
 @RestController
 @CrossOrigin
 @AllArgsConstructor
@@ -24,6 +29,15 @@ public class StudentController {
     private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
 
 
+
+    @ApiOperation(value = "Create a student" , response = StudentView.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 400, message = "Validation Exception"),
+            @ApiResponse(code = 404, message = "Resource Not Found Exception"),
+            @ApiResponse(code = 500, message = "Internal Exception")
+
+    })
     @PostMapping(value = "/register")
     public ResponseEntity<String> register(@RequestBody Student student) {
         String a = studentService.register(student);
